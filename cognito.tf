81828da1ac56808f2b54d48cce90bbcc67d59b40
vinay kumar- <vchinnakotla@quinovas.com>
2018-06-01 12:34:14 +0530

    made some small changes

--- cognito.tf
+++ cognito.tf
@@ -1,30 +1,3 @@
-resource "aws_iam_role" "main" {
-  name = "terraform-example-lambda"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_lambda_function" "main" {
-  filename      = "lambda.zip"
-  function_name = "terraform-example"
-  role          = "${aws_iam_role.main.arn}"
-  handler       = "exports.example"
-  runtime       = "nodejs4.3"
-}
 
 resource "aws_iam_role" "cidp" {
   name = "terraform-example-cognito-idp"
@@ -98,18 +71,7 @@ resource "aws_cognito_user_pool" "pool" {
     require_uppercase = true
   }
 
-  lambda_config {
-    create_auth_challenge          = "${aws_lambda_function.main.arn}"
-    custom_message                 = "${aws_lambda_function.main.arn}"
-    define_auth_challenge          = "${aws_lambda_function.main.arn}"
-    post_authentication            = "${aws_lambda_function.main.arn}"
-    post_confirmation              = "${aws_lambda_function.main.arn}"
-    pre_authentication             = "${aws_lambda_function.main.arn}"
-    pre_sign_up                    = "${aws_lambda_function.main.arn}"
-    pre_token_generation           = "${aws_lambda_function.main.arn}"
-    user_migration                 = "${aws_lambda_function.main.arn}"
-    verify_auth_challenge_response = "${aws_lambda_function.main.arn}"
-  }
+ 
 
   schema {
     attribute_data_type      = "String"
@@ -146,4 +108,3 @@ resource "aws_cognito_user_pool" "pool" {
     "Name"    = "FooBar"
     "Project" = "Terraform"
   }
-}
